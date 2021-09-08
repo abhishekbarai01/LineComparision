19bdf8a2fabcaa3e1bb7896a9eca9669722b4b00
Abhishek <abhishekb042@gmail.com>
2021-09-08 13:04:41 +0530

    This is a code to Check length of two line are equal or not

--- src/com/bridgelabz/LineComaparasion.java
+++ src/com/bridgelabz/LineComaparasion.java
@@ -4,22 +4,39 @@ import java.util.Scanner;
 
 class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Line Comparison ");
         Scanner sc = new Scanner(System.in);
         System.out.println("Value of x1: ");
-        double x1  = sc.nextDouble();
+        double x1 = sc.nextDouble();
         System.out.println("Value of x2: ");
-        double x2  = sc.nextDouble();
+        double x2 = sc.nextDouble();
         System.out.println("Value of y1: ");
-        double y1  = sc.nextDouble();
+        double y1 = sc.nextDouble();
         System.out.println("Value of y2: ");
-        double y2  = sc.nextDouble();
+        double y2 = sc.nextDouble();
 
         double a = x2 - x1;
         double b = y2 - y1;
 
-        double length_of_line = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        System.out.println(length_of_line);
+        System.out.println("Value of m1: ");
+        double m1 = sc.nextDouble();
+        System.out.println("Value of m2: ");
+        double m2 = sc.nextDouble();
+        System.out.println("Value of n1: ");
+        double n1 = sc.nextDouble();
+        System.out.println("Value of n2: ");
+        double n2 = sc.nextDouble();
+
+        double p = m2 - m1;
+        double q = n2 - n1;
+
+        double length_of_line1 = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        double length_of_line2 = Math.sqrt(Math.pow(p, 2) + Math.pow(q, 2));
+
+        if (length_of_line1 == length_of_line2) {
+            System.out.println("length of both line is equal");
+        } else {
+            System.out.println("length of both line is not equal");
+        }
     }
 }
 
